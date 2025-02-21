468965b4827c1e634dc3dc5de8683c0e806ea28d
malikbarbier7 <159799378+malikbarbier7@users.noreply.github.com>
2025-02-21 14:25:10 +0100

    gooood job

--- app/page.tsx
+++ app/page.tsx
@@ -1,100 +1,71 @@
 import Image from "next/image";
+import clsx from 'clsx';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="max-w-3xl mx-auto py-8 px-4 lg:py-12 min-h-screen flex flex-col gap-12">
+      <header className="flex flex-col gap-4">
+        <h1 className="font-caption text-2xl lg:text-3xl font-bold"> Malik Barbier</h1>
+      </header>
+      <p>
+        👋 Hi, I'm @malik <br />
+        👀 I'm interested in tech, environment, art, aviation, astronomy, philosophy...<br />
+        🌱 I'm currently a data Analyst but I am also coding random stuff as a hobby <br />
+        ✍️ I also like to write about subjects I find interesting
+      </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <main className="flex-1 flex flex-col gap-6 lg:gap-8">
+        <div className="flex flex-col gap-4">
+          <h2 className="font-caption text-lg lg:text-xl font-bold">Projects</h2>
+          <ul className="list-disc list-inside">
+            <li>
+              <span className="font-semibold">Cute pomodoro timer </span>
+              <a className="text-blue-500 font-semibold hover:underline" href="https://github.com/malikbarbier7/tomatotime">Tomato Time</a>
+              <span className="text-neutral-700"> 02-2025</span>
+            </li>
+
+            <li>
+              <span className="font-semibold">To do list app </span>
+              <a className="text-blue-500 font-semibold hover:underline" href="https://github.com/malikbarbier7/better-todo">Better To-do</a>
+              <span className="text-neutral-700"> 11-2024</span>
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <h2 className="font-caption text-lg lg:text-xl font-bold">Writing</h2>
+          <ul className="list-disc list-inside">
+            <li>
+              <a>The Rise and Fall of the Fossil Fuel Empire</a>
+              <span className="text-neutral-700 italic"> - Energy</span>
+              <a className="text-blue-500 font-semibold hover:underline" href="https://github.com/malikbarbier7/tomatotime"> - read</a>
+            </li>
+
+            <li>
+              <a>The Hidden Costs of Our Economy: Why Money Doesn't Reflect Reality</a>
+              <span className="text-neutral-700 italic"> - Energy</span>
+              <a className="text-blue-500 font-semibold hover:underline" href="https://github.com/malikbarbier7/tomatotime"> - read</a>
+            </li>
+
+            <li>
+              <a>The Illusion of Free Will: Are We Truly in Control ?</a>
+              <span className="text-neutral-700 italic"> - Philosophy</span>
+              <a className="text-blue-500 font-semibold hover:underline" href="https://github.com/malikbarbier7/tomatotime"> read</a>
+            </li>
+
+            <li>
+              <span className="font-semibold">For the latest updates, check my </span>
+              <a className="text-blue-500 font-semibold hover:underline" href="https://github.com/malikbarbier7/tomatotime">blog</a>
+              <span className="text-neutral-700"></span>
+            </li>
+          </ul>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      <footer className="flex items-center gap-4 lg:gap-8 flex-wrap text-neutral-700">
+        <a className="text-neutral-700 hover:underline" href="https://github.com/malikbarbier7/tomatotime/settings">Github</a>
+        <a className="text-neutral-700 hover:underline" href="https://x.com/Malikbuilds">X</a>
+        <a className="text-neutral-700 hover:underline" href="https://www.linkedin.com/in/malikbarbier/">LinkedIn</a>
       </footer>
     </div>
   );
