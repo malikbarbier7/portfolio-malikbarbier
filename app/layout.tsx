c1709b6b59abdfdc3fd8d6b0d871cffd2f7d27c9
malikbarbier7 <159799378+malikbarbier7@users.noreply.github.com>
2025-02-27 21:53:58 +0900

    fixing &apos

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Space_Grotesk } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -11,11 +11,6 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
-// const spaceGrotesk = Space_Grotesk({
-//   variable: "--font-space-grotesk",
-//   subsets: ["latin"],
-// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
